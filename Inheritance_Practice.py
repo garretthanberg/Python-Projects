b9adf2b9037aa7fca5daeef16e93e71762c1e7b2
garretthanberg <garretthanbergphoto@gmail.com>
2023-01-10 10:52:40 -0800

    Create Inheritance_Practice.py

---
+++ Inheritance_Practice.py
@@ -0,0 +1,20 @@
+
+#This is the parent class.
+class Vehicle:
+    Engine_Type = 'V8 Engine'
+    Steering_System = 'Steering Wheel'
+    Braking_System = 'Brake Pedal'
+    Number_of_Seats = 5
+
+#This is the first child class adding on two of its own attributes.
+class Airplane(Vehicle):
+    Number_of_Wing_Flaps = 2
+    Avionics_Switch = 'On'
+
+#This is the second child class adding on two of its own attributes.
+class Motorcycle(Vehicle):
+    Number_of_Blades = 7
+    Carburetor_Heat = 'Off'
+    
+    
+    
